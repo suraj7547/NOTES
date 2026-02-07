4be177b348f24f85c94a4545eb18b81c50ba07f8
suraj7547 <139971831+suraj7547@users.noreply.github.com>
2026-02-07 22:07:32 +0530

    Update C Language.c

--- C Language.c
+++ C Language.c
@@ -9,3 +9,33 @@ for (int i = 0; i < n-1; i++){
     }
   }
 }
+
+//insertion sort
+#include <stdio.h>
+
+void swap(int *a,int *b){
+    int temp;
+    temp=*a;
+    *a=*b;
+    *b=temp;
+}
+
+void printing(int arr[],int n){
+    for(int i=0;i<n-1;i++){
+        printf("%d ",arr[i]);
+    }
+}
+
+int main(){
+    int arr[]={1,5,3,0,2};
+    int n=5;
+    for(int i=1;i<=n-1;i++){
+        int j=i;
+        while(j>0 && arr[j]<arr[j-1]){
+            swap(&arr[j],&arr[j-1]);
+            j--;
+        }
+    }
+    printing(arr,n);
+    return 0;
+}
